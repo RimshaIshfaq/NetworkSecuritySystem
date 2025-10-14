0341e83d9c54d57c79e22277b8f1f2ebaef34f21
RimshaIshfaq <rimshaishfaq86@gmail.com>
2025-10-14 15:58:24 +0500

    Evaluation and MLflow committed

--- NetworkSecurity/components/model_trainer.py
+++ NetworkSecurity/components/model_trainer.py
@@ -19,6 +19,7 @@ from sklearn.ensemble import (
     GradientBoostingClassifier,
     RandomForestClassifier
 )
+import mlflow
 
 class ModelTrainer:
     def __init__(self,model_trainer_config: ModelTrainerConfig,data_transformation_artifact: DataTransformationArtifact):
@@ -28,6 +29,19 @@ class ModelTrainer:
             self.model_trainer_artifact=None
         except Exception as e:
             raise NetworkSecurityException(e,sys)
+        
+    def track_mlflow(self,best_model, classificationmetric):
+        with mlflow.start_run():
+            f1_score=classificationmetric.f1_score
+            precision_score=classificationmetric.precision_score
+            recall_score=classificationmetric.recall_score
+
+            mlflow.log_metric("f1_score", f1_score)
+            mlflow.log_metric("precision_Score", precision_score)
+            mlflow.log_metric("recall_score", recall_score)
+            mlflow.sklearn.log_model(best_model, "model")
+
+
     
     def train_model(self,x_train,y_train,x_test,y_test):
         models={
@@ -82,12 +96,15 @@ class ModelTrainer:
 
         classification_train_metric = get_classification_score(y_true=y_train, y_pred=y_train_pred)
 
-        #Track the mlflow
+        #Track experiemnts with the mlflow
 
         self.track_mlflow(best_model, classification_train_metric)
 
         y_test_pred=best_model.predict(x_test)
-        classification_test_model=get_classification_score(y_true=y_test, y_pred=y_test_pred)
+        classification_test_metric=get_classification_score(y_true=y_test, y_pred=y_test_pred)
+
+
+        self.track_mlflow(best_model, classification_test_metric)
 
         preprocessor = load_object(file_path=self.data_transformation_artifact.transformed_object_file_path)
 
@@ -99,7 +116,7 @@ class ModelTrainer:
         model_trainer_artifact = ModelTrainerArtifact(
             trained_model_file_path=self.model_trainer_config.trained_model_file_path,
             train_metric_artifact=classification_train_metric,
-            test_metric_artifact=classification_test_model
+            test_metric_artifact=classification_test_metric
         )
 
         logging.info(f"Model trainer artifact: {model_trainer_artifact}")
